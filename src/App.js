e5a36f11ee5dc8b2fd47d480f82ace83b06721a0
Sailee30 <saileechavan04@gmail.com>
2025-10-28 12:46:05 -0700

    Fix App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import ForgotPassword from "./pages/ForgotPassword";
+import ChangePassword from "./pages/ChangePassword";
+import Doubt from "./pages/Doubt";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/su" element={<SignUp />} />
+        <Route path="/ho" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="/au" element={<ProtectedRoute><AboutUs /></ProtectedRoute>} />
+        <Route path="/fp" element={<ForgotPassword />} />
+        <Route path="/cp" element={<ProtectedRoute><ChangePassword /></ProtectedRoute>} />
+        <Route path="/do" element={<ProtectedRoute><Doubt /></ProtectedRoute>} />
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
